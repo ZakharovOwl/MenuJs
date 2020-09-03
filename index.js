848b4c9833307e39d4d8957d5cdcb5c80281f2ea
Александр Захаров <a.zaharov@transimperial.local>
2020-09-03 11:28:45 +0300

    feat: add full menu

--- index.js
+++
@@ -1,8 +0,0 @@
-const bars = document.querySelector('.bars');
-        const navBar = document.querySelector('#navbar');
-
-        bars.addEventListener('click', ()=> {
-        navBar.classList.toggle('active');
-        bars.classList.toggle('rotate') ; 
-})
-        
---
+++ src/index.js
@@ -0,0 +1,23 @@
+const bars = document.querySelector('.bars');
+const navBar = document.querySelector('#navbar');
+
+const barsMenuOne = document.querySelector('.bars-menu-one');
+const menuOne = document.querySelector('#menu-one'); 
+
+const barsMenuTwo = document.querySelector('.bars-menu-two');
+const menuTwo = document.querySelector('#menu-two'); 
+
+
+
+        bars.addEventListener('click', ()=> {
+        navBar.classList.toggle('active');
+        bars.classList.toggle('rotate') ;     
+})
+        barsMenuOne.addEventListener('click', ()=> {
+        menuOne.classList.toggle('active-menu-one');
+        barsMenuOne.classList.toggle('rotate') ;      
+})
+        barsMenuTwo.addEventListener('click', ()=> {
+        menuTwo.classList.toggle('active-menu-two');
+        barsMenuTwo.classList.toggle('rotate');      
+})
